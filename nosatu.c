193d515352e42fd4251e5098c4c49d56c111dac6
mahathirm <mahathirmi@gmail.com>
2017-11-24 10:37:07 +0700

    revisi

---
+++ nosatu.c
@@ -0,0 +1,122 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+static const char *dirpath = "/home/hajarblehh/Documents";
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+  int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+  char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		res = (filler(buf, de->d_name, &st, 0));
+			if(res!=0) break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+  char fpath[1000];
+	char c[2000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+ 	int fd = 0 ;
+	int flag=0;
+	
+		
+
+	if((strstr(fpath,".pdf")>0) flag =1 
+	else if((strstr(fpath,".doc")>0) flag =1;
+	else if ((strstr(fpath,".txt")>0) flag =1;
+	
+	if (flag == 1)	
+	{
+		char buf[BUFSIZ];
+		char warning[]="Terjadi\\ kesalahan!\\ File\\ berisi\\ konten\\ berbahaya.";
+		snprintf(buf,sizeof(buf),"zenity --error --text=%s",warning);
+		system(buf);
+		
+		char newnamefile[100];
+		strcpy(newnamefile,fpath);
+		strcat(newnamefile,".ditandai");
+		rename(fpath,newnamefile);
+	}
+	else
+	{	
+		(void) fi;
+		fd = open(fpath, O_RDONLY);
+		if (fd == -1)
+			return -errno;
+
+		res = pread(fd, buf, size, offset);
+		if (res == -1)
+			res = -errno;
+
+		close(fd);
+		return res;
+	}
+}
+
+static struct fuse_operations xmp_oper = {
+	.getattr	= xmp_getattr,
+	.readdir	= xmp_readdir,
+  	.read		= xmp_read,
+	.rename		= xmp_rename,
+};
+
+int main(int argc, char *argv[])
+{
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
+}
